55070191eb7c73c59b76e35211ae254e2eac085e
Long <long05012001@gmail.com>
2022-05-15 22:41:03 +0700

    update logo

--- src/components/layout/footer/index.js
+++ src/components/layout/footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="footer">
         <div className="footer-content container">
           <Link to="/">
-            <img src={LogoWhite} alt="" />
+            <img src={LogoWhite} alt=""/>
           </Link>
           <div className="footer-column">
             <h3>Thông tin website</h3>
@@ -22,24 +22,23 @@ const Footer = () => {
           <div className="footer-column">
             <h3>Hotline</h3>
             <div>
-              <Link to="/products">Đặt hàng 1800 6936 (07:00-20:30)</Link>
-              <Link to="/">Hỗ trợ 028.71.087.088 (07:00-21:00)</Link>
+              <Link to="/products">Đặt hàng: 0376791957 (07:00-20:30)</Link>
+              <Link to="/">Hỗ trợ: 0376791957 (07:00-21:00)</Link>
             </div>
           </div>
           <div className="footer-column">
             <h3>Liên hệ</h3>
             <div>
               <Link to="/">
-                Địa chỉ: 86 - 88 Cao Thang, Ward 4, District 3, Ho Chi Minh,
-                Vietnam
+                Địa chỉ: 96A Trần Phú, P. Mộ Lao, Hà Đông, Hà Nội
               </Link>
-              <Link to="/">Điện thoại: +842871 078 079</Link>
+              <Link to="/">Điện thoại: +8437 67 919 57</Link>
             </div>
           </div>
         </div>
       </div>
       <div className="copy-right">
-        Copyright © 2021 The Coffee House. All rights reserved.
+        Copyright © 2022 The Coffee Shop. All rights reserved.
       </div>
     </footer>
   );
