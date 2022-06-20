fffd3a3ab0c51d0d8d91976eea133785609e8fc4
Trần Văn Thắng <bareversewin2000@gmail.com>
2022-06-21 00:21:13 +0700

    just keep admin

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/layout";
 import Home from "./pages/home";
-import News from "./pages/news";
-import Order from "./pages/order";
 import "./App.scss";
-import OrderDetail from "./pages/order/detail";
-import NewsDetail from "./pages/news/detail";
-import Checkout from "./pages/checkout";
 import AdminPage from "./pages/admin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" />
-          <Route path="blogs">
-            <Route path=":idNews" element={<NewsDetail />} />
-            <Route path="" element={<News />} />
-          </Route>
-          <Route path="products">
-            <Route path=":idProduct" element={<OrderDetail />} />
-            <Route path="" element={<Order />} />
-          </Route>
-          <Route path="checkout" element={<Checkout />} />
-          <Route path="admin" element={<AdminPage />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="admin" element={<AdminPage />} />
       </Routes>
     </BrowserRouter>
   );
